bd9e9898a1778f8b4a43110972b9edd56ed0d306
1337DotID <1337DotID@users.noreply.github.com>
2016-12-23 12:41:54 +0100

    Added files for ArduinoJoystickLibrary v2.0
    You can use version 1.0.1 or 2.0 of the ArduinoJoystickLibrary. Just make sure you are using the right version of this sketch.

---
+++ joystick_v2.0/joystick_v2.0.ino
@@ -0,0 +1,75 @@
+#include "Joystick.h"
+
+Joystick_ Joystick;
+
+int throttle = 0;
+int throttleValue;
+int throttleValueOld;
+
+int yaw = 1;
+int yawValue;
+int yawValueOld;
+
+int pitch = 2;
+int pitchValue;
+int pitchValueOld;
+
+int roll = 3;
+int rollValue;
+int rollValueOld;
+
+void setup() {
+  attachInterrupt(throttle, throttleRising, RISING);
+  attachInterrupt(yaw, yawRising, RISING);
+  attachInterrupt(pitch, pitchRising, RISING);
+  attachInterrupt(roll, rollRising, RISING);
+  
+  Joystick.begin(true);
+}
+
+void throttleRising() {
+  attachInterrupt(throttle, throttleFalling, FALLING);
+  throttleValueOld = micros();
+}
+
+void throttleFalling() {
+  attachInterrupt(throttle, throttleRising, RISING);
+  throttleValue = micros() - throttleValueOld;
+}
+
+void yawRising() {
+  attachInterrupt(yaw, yawFalling, FALLING);
+  yawValueOld = micros();
+}
+
+void yawFalling() {
+  attachInterrupt(yaw, yawRising, RISING);
+  yawValue = micros() - yawValueOld;
+}
+
+void pitchRising() {
+  attachInterrupt(pitch, pitchFalling, FALLING);
+  pitchValueOld = micros();
+}
+
+void pitchFalling() {
+  attachInterrupt(pitch, pitchRising, RISING);
+  pitchValue = micros() - pitchValueOld;
+}
+
+void rollRising() {
+  attachInterrupt(roll, rollFalling, FALLING);
+  rollValueOld = micros();
+}
+
+void rollFalling() {
+  attachInterrupt(roll, rollRising, RISING);
+  rollValue = micros() - rollValueOld;
+}
+
+void loop() {
+  Joystick.setYAxis(map(throttleValue, 1050, 1960, 512, 1023));
+  Joystick.setXAxis(map(yawValue, 1050, 1960, 512, 1023));
+  Joystick.setZAxis(map(pitchValue, 1050, 1960, 512, 1023));
+  Joystick.setRxAxis(map(rollValue, 1050, 1960, 512, 1023));
+}
